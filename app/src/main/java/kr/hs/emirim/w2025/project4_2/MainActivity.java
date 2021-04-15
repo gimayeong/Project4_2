b4b184263201e4bdea29b988af869ef16c8ef528
w2025 <w2025@e-mirim.hs.kr>
2021-04-15 19:16:40 +0900

    직접풀어보기 4-4 시작함 스위치로 변경

--- app/src/main/java/kr/hs/emirim/w2025/project4_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2025/project4_2/MainActivity.java
@@ -3,12 +3,59 @@ package kr.hs.emirim.w2025.project4_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switchStart;
+    RadioGroup rg;
+    ImageView imgv;
+    LinearLayout linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchStart = findViewById(R.id.switch_start);
+        linear = findViewById(R.id.linear);
+        rg = findViewById(R.id.rg);
+        imgv = findViewById(R.id.imgv);
+        Button btnDone = findViewById(R.id.btn_done);
+
+        switchStart.setOnCheckedChangeListener(checkListener);
+        btnDone.setOnClickListener(btnListener);
+
     }
+    CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(isChecked){
+                linear.setVisibility(View.VISIBLE);
+            }else{
+                linear.setVisibility(View.INVISIBLE);
+            }
+        }
+    };
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.radio_dog:
+                    imgv.setImageResource(R.drawable.dog);
+                    break;
+                case R.id.radio_cat:
+                    imgv.setImageResource(R.drawable.cat);
+                    break;
+                case R.id.radio_rabbit:
+                    imgv.setImageResource(R.drawable.rabbit);
+                    break;
+            }
+        }
+    };
 }
